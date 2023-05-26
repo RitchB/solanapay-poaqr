a25361a44c79e4b3a3c520caa727dee7a529769a
ritchb <ritchb001@hotmail.com>
2023-05-25 22:41:12 -0400

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,24 @@
-// app/page.tsx
 "use client";
 
-import { useEffect, useState, useRef, RefObject, MutableRefObject } from 'react';
+import { useEffect, useRef, RefObject } from 'react';
 import Link from 'next/link';
 
 import { createQR } from "@solana/pay";
 const SOLANA_PAY_URL = "solana:http://localhost:3000/api/poaqr";
 
 export default function Home() {
-
-  //const qrRef = useRef<HTMLDivElement>()
   const qrRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
-  
+
   useEffect(() => {
     const qr = createQR(SOLANA_PAY_URL, 360, 'white', 'orange');
 
     // Set the generated QR code on the QR ref element
     if (qrRef.current) {
-      qrRef.current.innerHTML = ''
-      qr.append(qrRef.current)
+      qrRef.current.innerHTML = '';
+      qr.append(qrRef.current);
       console.log("appended");
     }
-  }, [])
+  }, []);
 
   return (
     <div className="md:hero mx-auto p-4 flex justify-center items-center h-screen">
@@ -30,7 +27,7 @@ export default function Home() {
         <div className='mt-6'>
           <div className='text-sm font-normal align-bottom text-center text-slate-600 mt-4'>Scan this</div>
 
-          <div ref={qrRef} />
+          <div ref={qrRef}></div>
 
         </div>
       </div>
