15801279412c1523a2add95af65a2837866d2a3d
ritchb <ritchb001@hotmail.com>
2023-06-03 21:58:03 -0400

    update 39

--- pages/api/poaqrcnft.tsx
+++ pages/api/poaqrcnft.tsx
@@ -91,7 +91,7 @@ async function post(
     requireAllSignatures: false,
   }));
 
-  transaction.sign(authority.publicKey);
+  transaction.sign(authority);
   console.log('authority qui est dans le transaction.sign', authority)
   console.log('transaction', transaction)
 
@@ -101,12 +101,16 @@ async function post(
     verifySignatures: false,
     requireAllSignatures: false,
   });
+  console.log('serializedTransaction:', serializedTransaction)
 
   const base64Transaction = serializedTransaction.toString('base64');
+  console.log('base64Transaction:', base64Transaction)
   const message = 'Thank you for minting with SolRitchB!';
+  console.log('message:', message)
 
 
   res.status(200).send({ transaction: base64Transaction, message });
+  console.log('yolo')
 
 }
 
@@ -191,6 +195,7 @@ async function createMintCNFTInstruction(merkleTree: PublicKey, account: PublicK
       tokenProgramVersion: TokenProgramVersion.Original
     }
   });
+  console.log('ix createMintToCollectionV1Instruction:', ix)
 
   return ix;
 }
